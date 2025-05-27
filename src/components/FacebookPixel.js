7e295d98c2ea7b776e9ae69a0d1d79f2cc80ea9f
ianjj2 <standedacademy@gmail.com>
2025-05-27 16:51:27 -0300

    Update FacebookPixel.js

--- src/components/FacebookPixel.js
+++ src/components/FacebookPixel.js
@@ -7,34 +7,26 @@ const FacebookPixel = () => {
   useEffect(() => {
     // Carrega o script do Facebook Pixel
     const loadFacebookPixel = () => {
-      !function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)}(window, document,'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
-
-      // Inicializa o Pixel
-      fbq('init', '1002539111981798');
-      fbq('track', 'PageView');
+      const script = document.createElement('script');
+      script.innerHTML = `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '${process.env.REACT_APP_FACEBOOK_PIXEL_ID}');
+        fbq('track', 'PageView');
+      `;
+      document.head.appendChild(script);
     };
 
     loadFacebookPixel();
   }, []);
 
-  return (
-    <noscript>
-      <img
-        height="1"
-        width="1"
-        style={{ display: 'none' }}
-        src="https://www.facebook.com/tr?id=1002539111981798&ev=PageView&noscript=1"
-        alt=""
-      />
-    </noscript>
-  );
+  return null;
 };
 
 export default FacebookPixel; 
