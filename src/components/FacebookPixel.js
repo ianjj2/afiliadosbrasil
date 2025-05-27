f2f15f2e279b521b10810b9670c342839558b578
ianjj2 <standedacademy@gmail.com>
2025-05-27 16:53:39 -0300

    Update FacebookPixel.js

--- src/components/FacebookPixel.js
+++ src/components/FacebookPixel.js
@@ -6,24 +6,37 @@ import React, { useEffect } from 'react';
 const FacebookPixel = () => {
   useEffect(() => {
     // Carrega o script do Facebook Pixel
-    const loadFacebookPixel = () => {
-      const script = document.createElement('script');
-      script.innerHTML = `
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '${process.env.REACT_APP_FACEBOOK_PIXEL_ID}');
-        fbq('track', 'PageView');
-      `;
-      document.head.appendChild(script);
-    };
+    const script = document.createElement('script');
+    script.innerHTML = `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1002539111981798');
+      fbq('track', 'PageView');
+    `;
+    document.head.appendChild(script);
 
-    loadFacebookPixel();
+    // Adiciona o noscript fallback
+    const noscript = document.createElement('noscript');
+    const img = document.createElement('img');
+    img.height = 1;
+    img.width = 1;
+    img.style.display = 'none';
+    img.src = 'https://www.facebook.com/tr?id=1002539111981798&ev=PageView&noscript=1';
+    img.alt = '';
+    noscript.appendChild(img);
+    document.body.appendChild(noscript);
+
+    return () => {
+      // Cleanup
+      document.head.removeChild(script);
+      document.body.removeChild(noscript);
+    };
   }, []);
 
   return null;
